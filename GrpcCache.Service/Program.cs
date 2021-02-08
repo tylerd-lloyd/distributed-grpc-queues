e80c065b0e70f58fb1a963eb1a571489ddbdaa12
Tyler Lloyd <tylloyd@microsoft.com>
2021-02-08 07:44:43 -0500

    Cleaning up using statements

--- GrpcCache.Service/Program.cs
+++ GrpcCache.Service/Program.cs
@@ -1,12 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace GrpcCache.Service
 {
     public class Program
